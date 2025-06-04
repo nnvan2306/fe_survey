b351f1c21566ac1d35d1395198885f2d2417e2e9
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2025-06-04 23:14:45 +0700

    up

--- src/components/organisms/Rating/Rating.tsx
+++ src/components/organisms/Rating/Rating.tsx
@@ -2,8 +2,15 @@ import "./styles.scss";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import FreeBreakfastIcon from "@mui/icons-material/FreeBreakfast";
+import CircleIcon from "@mui/icons-material/Circle";
+import CheckIcon from "@mui/icons-material/Check";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import RadioButtonCheckedIcon from "@mui/icons-material/RadioButtonChecked";
 import type { OptionType, QuestionType } from "../../../types/survey";
-import { useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 
 type Props = {
     question: QuestionType;
@@ -24,10 +31,73 @@ const Rating = ({ question, handleUpdateQuestion }: Props) => {
         [question?.configJsonString?.ratingLength]
     );
 
+    const handleRenderIcons = useCallback(() => {
+        const type = question?.configJsonString?.ratingIcon || "";
+        switch (type) {
+            case "FavoriteIcon":
+                return (
+                    <FavoriteIcon
+                        fontSize="large"
+                        className="cursor-pointer text-gray-700"
+                    />
+                );
+            case "ThumbUpIcon":
+                return (
+                    <ThumbUpIcon
+                        fontSize="large"
+                        className="cursor-pointer text-gray-700"
+                    />
+                );
+            case "FreeBreakfastIcon":
+                return (
+                    <FreeBreakfastIcon
+                        fontSize="large"
+                        className="cursor-pointer text-gray-700"
+                    />
+                );
+            case "CircleIcon":
+                return (
+                    <CircleIcon
+                        fontSize="large"
+                        className="cursor-pointer text-gray-700"
+                    />
+                );
+            case "CheckIcon":
+                return (
+                    <CheckIcon
+                        fontSize="large"
+                        className="cursor-pointer text-gray-700"
+                    />
+                );
+            case "RocketLaunchIcon":
+                return (
+                    <RocketLaunchIcon
+                        fontSize="large"
+                        className="cursor-pointer text-gray-700"
+                    />
+                );
+            case "RadioButtonCheckedIcon":
+                return (
+                    <RadioButtonCheckedIcon
+                        fontSize="large"
+                        className="cursor-pointer text-gray-700"
+                    />
+                );
+            default:
+                return (
+                    <StarBorderIcon
+                        fontSize="large"
+                        className="cursor-pointer text-gray-700"
+                    />
+                );
+        }
+    }, [question]);
+
     useEffect(() => {
         if (!question?.configJsonString?.ratingLength) {
             handleUpdateQuestion("configJsonString", {
-                ratingIcon: question?.configJsonString?.ratingIcon ?? "star",
+                ratingIcon:
+                    question?.configJsonString?.ratingIcon ?? "StarBorderIcon",
                 ratingLength: 5,
             });
         }
@@ -38,10 +108,7 @@ const Rating = ({ question, handleUpdateQuestion }: Props) => {
             <Box className="flex space-x-4">
                 {Array.from({ length: count }).map((_, index) => (
                     <Box key={index} className="flex flex-col items-center">
-                        <StarBorderIcon
-                            fontSize="large"
-                            className="cursor-pointer text-gray-700"
-                        />
+                        {handleRenderIcons()}
                         <Typography
                             variant="body2"
                             className="text-gray-700 mt-1"
