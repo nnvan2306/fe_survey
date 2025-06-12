16029b81c6a8ff897c775ea7add7c01fdd163e8c
Truong Son Dev <truongsonpt.80@gmail.com>
2025-06-12 23:05:29 +0700

    updateupdate

--- src/components/molecules/voice/Voice.tsx
+++ src/components/molecules/voice/Voice.tsx
@@ -49,10 +49,22 @@ const Voice = ({
             showCancelButton: true,
             confirmButtonText: "Yes",
             html: `
-                <div>
-                <p><strong>Pro</strong></p>
-                <p>Random captcha giữa những câu hỏi. Random Re-question câu hỏi bất kỳ cho bài khảo sát. Chủ động điều chỉnh Time-limit cho từng câu hỏi. Cơ chế Jump Logic giúp khảo sát được liền mạch và chắt lọc thông tin hơn. Tính năng set voice-answer cho câu hỏi</p>
-            </div>
+                <div style="text-align: left; padding: 15px; font-family: Arial, sans-serif;">
+                    <div style="margin-bottom: 15px;">
+                        <p style="font-size: 16px; font-weight: bold; color: #333;">Basic</p>
+                        <p style="font-size: 14px;">6 loại câu hỏi để bạn tha hồ tùy chỉnh khảo sát. Random captcha giữa những câu hỏi. Random Time-limit cho câu hỏi.</p>
+                    </div>
+
+                    <div style="margin-bottom: 15px;">
+                        <p style="font-size: 16px; font-weight: bold; color: #333;">Advance</p>
+                        <p style="font-size: 14px;">6 loại câu hỏi để bạn tha hồ tùy chỉnh khảo sát. Random captcha giữa những câu hỏi. Random Re-question câu hỏi bất kỳ cho bài khảo sát. Chủ động điều chỉnh Time-limit cho từng câu hỏi. Cơ chế Jump Logic giúp khảo sát được liền mạch và chắt lọc thông tin hơn.</p>
+                    </div>
+
+                    <div style="background-color: #4caf50; color: #ffffff; padding: 10px; border-radius: 5px;">
+                        <p style="font-size: 16px; font-weight: bold;">Pro</p>
+                        <p style="font-size: 14px;">6 loại câu hỏi để bạn tha hồ tùy chỉnh khảo sát. Random captcha giữa những câu hỏi. Random Re-question câu hỏi bất kỳ cho bài khảo sát. Chủ động điều chỉnh Time-limit cho từng câu hỏi. Cơ chế Jump Logic giúp khảo sát được liền mạch và chắt lọc thông tin hơn. Tính năng set voice-answer cho câu hỏi.</p>
+                    </div>
+                </div>
             `
         }).then((result) => {
             if (result.isConfirmed) {
